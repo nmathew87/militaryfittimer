a7ea9303c759d2beb250d6a78d1e4d1e10ccd0e0
Neil <nmathew87@gmail.com>
2020-07-19 18:03:16 -0400

    updating set times

--- workouts.js
+++ workouts.js
@@ -5,55 +5,55 @@ var workouts = [{
         { 
             "name": "High Knees",
             "image": "assets/images/highknees.jpg",
-            "time": "6",
+            "time": "60",
             "count": "0"
         },
         {
             "name": "20 Pushups",
             "image": "assets/images/pushup.jpg",
-            "time": "6",
+            "time": "60",
             "count": "20"
         },
         {
             "name": "20 Shoulder Taps",
             "image": "assets/images/shouldertaps.jpg",
-            "time": "6",
+            "time": "45",
             "count": "20"
         },
         {
             "name": "Plank Hold",
             "image": "assets/images/plankhold.jpg",
-            "time": "4",
+            "time": "30",
             "count": "0"
         },
         {
             "name": "20 Jumping Lunges",
             "image": "assets/images/jumpinglunges.jpg",
-            "time": "6",
+            "time": "45",
             "count": "20"
         },
         {
             "name": "20 Knee-ins + Twist",
             "image": "assets/images/kneeinsplustwist.png",
-            "time": "6",
+            "time": "45",
             "count": "20"
         },
         {
             "name": "20 Raised Leg Circles",
             "image": "assets/images/raisedlegcircles.jpg",
-            "time": "6",
+            "time": "45",
             "count": "20"
         },
         {
             "name": "20 Up and Down Planks",
             "image": "assets/images/upanddownplanks.jpg",
-            "time": "6",
+            "time": "60",
             "count": "20"
         },
         {
             "name": "Rest and Get a Drink",
             "image": "https://media.giphy.com/media/3ov9jW5LkqPw06WDCw/giphy.gif",
-            "time": "6",
+            "time": "120",
             "count": "20"
 
         }
